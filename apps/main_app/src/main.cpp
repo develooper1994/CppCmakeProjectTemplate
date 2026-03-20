60e7b6e1434afb1241d64162b7506f5cfc595fb8
Developer <Developer>
2026-03-20 20:44:31 +0300

    Fix CMakePresets JSON and update main to show compiler info

--- apps/main_app/src/main.cpp
+++ apps/main_app/src/main.cpp
@@ -5,6 +5,7 @@
 int main() {
     std::cout << build_info::project_name << " v" << build_info::project_version << "\n";
     std::cout << "Branch: " << build_info::git_branch << " Hash: " << build_info::git_hash << "\n";
+    std::cout << "Compiler: " << build_info::compiler_id << " v" << build_info::compiler_version << "\n";
     std::cout << dummy_lib::get_greeting() << "\n";
     return 0;
 }
