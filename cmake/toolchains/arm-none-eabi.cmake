a0c558b51d715e9ddecc7770b50223ae30bb8261
Developer <Developer>
2026-03-20 21:13:42 +0300

    Add professional Embedded/Cross-Compilation support with ARM toolchain template

--- cmake/toolchains/arm-none-eabi.cmake
+++ cmake/toolchains/arm-none-eabi.cmake
@@ -1,19 +1,32 @@
 # cmake/toolchains/arm-none-eabi.cmake
-# Example toolchain for ARM Cortex-M
+# Standard Toolchain for ARM Cortex-M (Bare Metal)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Specify the cross compiler
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+# 1. Setup paths to your local GNU ARM Toolchain
+# You can override these from the command line: -DCMAKE_C_COMPILER=...
+if(NOT CMAKE_C_COMPILER)
+    set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+endif()
+if(NOT CMAKE_CXX_COMPILER)
+    set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+endif()
 
-# Specify target flags
-set(OBJECT_GEN_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp")
-set(CMAKE_C_FLAGS "${OBJECT_GEN_FLAGS} ${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS "${OBJECT_GEN_FLAGS} ${CMAKE_CXX_FLAGS}")
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy CACHE INTERNAL "objcopy tool")
+set(CMAKE_SIZE    arm-none-eabi-size    CACHE INTERNAL "size tool")
 
-# Inhibit search in default paths
+# 2. Architecture specific flags (Example: Cortex-M4)
+set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+set(COMMON_FLAGS "${CPU_FLAGS} -ffunction-sections -fdata-sections -Wall")
+
+set(CMAKE_C_FLAGS   "${COMMON_FLAGS}" CACHE STRING "C flags")
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS}" CACHE STRING "C++ flags")
+
+# 3. Linker flags (Note: Linker script is usually required for bare metal)
+# set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -Wl,--gc-sections -T ${CMAKE_CURRENT_SOURCE_DIR}/linker.ld" CACHE STRING "Linker flags")
+
+# 4. Search policy
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
